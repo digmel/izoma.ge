43c1a9af7643a35baa02dc1de0cb988c75bca08d
George <gio.digmel@gmail.com>
2023-07-11 18:49:26 -0500

    fix: more clean up

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -21,10 +21,6 @@ export const HomePage = () => {
   return (
     <div className="Wrapper">
       <Header />
-      <Hero />
-      <About />
-      <Statistics />
-      <Blog />
       <Footer />
     </div>
   );
