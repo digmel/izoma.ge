c02a4e0079a8a628465e604f8d41b0fae88cad54
George <gio.digmel@gmail.com>
2023-07-11 20:19:20 -0500

    fix: about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,62 @@
 import React from "react";
 import { Button } from "../Button";
+import { Clockicon2 } from "../../icons/Clockicon2";
 import "./style.css";
-
-// TODO: install i18n for all text content and create translations.json file
 
 export const About = () => {
   return (
     <div className="about">
-      <div className="title">ვინ ვართ ჩვენ?</div>
-      <p className="description">
-        ბოლო რამდენიმე წელია ტრადიციად გვექცა რომ ლაშქრობის დროს მთაში მცხოვრები
-        ბავშვებისთვის წიგნები ჩავიტანოთ. მიღებულმა შედეგებმა გვაფიქრებინა, რომ
-        უფრო მასშტაბური გაგვეხადა ეს ყველაფერი. ახლა კი, მთიელი წიგნების
-        მიზანია, შევიძინოთ, შევაგროვოთ საინტერესო და სასურველი ლიტერატურა მთიელი
-        მოზარდებისათვის.
-      </p>
-      <Button requestAnEstimateClassName="button-3" text="დონაცია" />
+      <div className="description-5">
+        <div className="text-wrapper-5">ჩვენ შესახებ</div>
+        <div className="flex-container">
+          <div className="text">
+            <span className="span">
+              &#34;აიზომა&#34; დაკომპლექტებულია მაღალი პასუხისმგებლობის და
+              საქმეზე ორიენტირებული გეოდეზისტების ჯგუფით. <br />
+            </span>
+          </div>
+          <div className="text">
+            <span className="span">
+              ჩვენი გუნდი გაგიწევთ უფასო კონსულტაციას როგორც ტექნიკური ასევე
+              სამართლებლივი კუთხით. &#34;აიზომა&#34; საჯარო რეესტრის
+              ავტორიზებული მომხმარებელია, რაც იმას ნიშნავს რომ ჩვენი ოფისიდანვე
+              მოხდება საჯარო რეესტრში განაცხადის შეტანა სარეგისტრაციოდ, რაც
+              თავიდან აგაცილებთ იუსტიციის სახლში რიგში დგომას.
+            </span>
+          </div>
+        </div>
+        <div className="work-hours">
+          <Clockicon2 className="clock-icon" color="#6B7280" />
+          <p className="text-wrapper-6">
+            ვმუშაობთ ყოველ დღე 10:00-დან 18:00-მდე
+          </p>
+        </div>
+        <Button />
+      </div>
+      <div className="group">
+        <div className="rectangle-wrapper">
+          <img
+            className="rectangle-50"
+            alt="Rectangle"
+            src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5192-2@2x.png"
+          />
+        </div>
+        <img
+          className="rectangle-51"
+          alt="Rectangle"
+          src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5191-2@2x.png"
+        />
+        <img
+          className="rectangle-52"
+          alt="Rectangle"
+          src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5197-2@2x.png"
+        />
+        <img
+          className="rectangle-53"
+          alt="Rectangle"
+          src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5196-2@2x.png"
+        />
+      </div>
     </div>
   );
 };
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -11,6 +11,7 @@ import { IsmobFalseWrapper } from "../../components/IsmobFalseWrapper";
 import { ServicesSection } from "../../components/ServicesSection";
 import { Chevronicon3 } from "../../icons/Chevronicon3";
 import { Clockicon2 } from "../../icons/Clockicon2";
+import { About } from "../../components/About";
 
 export const HomePage = () => {
   const [isMob, setIsMob] = useState(false);
@@ -36,61 +37,10 @@ export const HomePage = () => {
       />
 
       <ServicesSection isMob={isMob} />
-      <div className="about-section">
-        <div className="description-5">
-          <div className="text-wrapper-5">ჩვენ შესახებ</div>
-          <div className="flex-container">
-            <div className="text">
-              <span className="span">
-                &#34;აიზომა&#34; დაკომპლექტებულია მაღალი პასუხისმგებლობის და
-                საქმეზე ორიენტირებული გეოდეზისტების ჯგუფით. <br />
-              </span>
-            </div>
-            <div className="text">
-              <span className="span">
-                ჩვენი გუნდი გაგიწევთ უფასო კონსულტაციას როგორც ტექნიკური ასევე
-                სამართლებლივი კუთხით. &#34;აიზომა&#34; საჯარო რეესტრის
-                ავტორიზებული მომხმარებელია, რაც იმას ნიშნავს რომ ჩვენი
-                ოფისიდანვე მოხდება საჯარო რეესტრში განაცხადის შეტანა
-                სარეგისტრაციოდ, რაც თავიდან აგაცილებთ იუსტიციის სახლში რიგში
-                დგომას.
-              </span>
-            </div>
-          </div>
-          <div className="work-hours">
-            <Clockicon2 className="clock-icon" color="#6B7280" />
-            <p className="text-wrapper-6">
-              ვმუშაობთ ყოველ დღე 10:00-დან 18:00-მდე
-            </p>
-          </div>
-          <Button />
-        </div>
-        <div className="group">
-          <div className="rectangle-wrapper">
-            <img
-              className="rectangle-50"
-              alt="Rectangle"
-              src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5192-2@2x.png"
-            />
-          </div>
-          <img
-            className="rectangle-51"
-            alt="Rectangle"
-            src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5191-2@2x.png"
-          />
-          <img
-            className="rectangle-52"
-            alt="Rectangle"
-            src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5197-2@2x.png"
-          />
-          <img
-            className="rectangle-53"
-            alt="Rectangle"
-            src="https://generation-sessions.s3.amazonaws.com/5dfcc5d69cc66bba85ef251c08b40128/img/rectangle-5196-2@2x.png"
-          />
-        </div>
-      </div>
-      <div className="FAQ">
+
+      <About />
+
+      {/* <div className="FAQ">
         <div className="text-wrapper-7">ხშირად დასმული კითხვები</div>
         <div className="faq">
           <div className="overlap-group-3">
@@ -208,7 +158,7 @@ export const HomePage = () => {
           </div>
         </div>
         <Button className="button-2" text="იხილე მეტი" />
-      </div>
+      </div> */}
       <IsmobFalseWrapper className="blog-section-instance" isMob={isMob} />
       <ContactSection
         className="contact-section-instance"
