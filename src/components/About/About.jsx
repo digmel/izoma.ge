f69eb3a235e5cfcfd1877021043503763c8203b9
George <gio.digmel@gmail.com>
2023-11-14 19:37:58 -0800

    chore: add SEO keywords

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,13 +4,12 @@ import { Clockicon2 } from "../../icons/Clockicon2";
 import "./style.css";
 
 export const About = ({ isMob }) => {
-
   const scrollToSection = (sectionId) => {
     const section = document.getElementById(sectionId);
     if (section) {
       window.scrollTo({
         top: section.offsetTop,
-        behavior: "smooth"
+        behavior: "smooth",
       });
     }
   };
@@ -81,24 +80,29 @@ export const About = ({ isMob }) => {
               <div className="overlap-group">
                 <img
                   className="rectangle"
-                  alt="Rectangle"
+                  alt="აზომვითი ნახაზის მომზადება, ბინის შიდა აზომვა"
                   src="https://generation-sessions.s3.amazonaws.com/fb4df792853a0609f29c896511e51542/img/rectangle-5192-1@2x.png"
                 />
               </div>
               <img
                 className="img"
-                alt="Rectangle"
+                alt="აზომვითი სამუშაოები, ბინის აზომვა, სახლის აზომვა"
                 src="https://generation-sessions.s3.amazonaws.com/fb4df792853a0609f29c896511e51542/img/rectangle-5191@2x.png"
               />
               <img
                 className="rectangle-2"
-                alt="Rectangle"
+                alt="ბინის აზომვითი ნახაზი, ნახაზების დამზადება, შიდა აზომვა, შიდა აზომვითი ნახაზი"
                 src="https://generation-sessions.s3.amazonaws.com/fb4df792853a0609f29c896511e51542/img/rectangle-5195-1@2x.png"
               />
               <img
                 className="rectangle-3"
-                alt="Rectangle"
+                alt="ბინის გამიჯვნა, ბინის შეერთება, სარდაფის აზომვა, სხვენის აზომვა, ნახევარსარდაფის აზომვა, ავტოფარეხის აზომვა, საკადასტრო გეგმის მომზადება, საკადასტრო აზომვითი ნახაზი"
                 src="https://generation-sessions.s3.amazonaws.com/fb4df792853a0609f29c896511e51542/img/rectangle-5196-1@2x.png"
+              />
+              <img
+                className="banner"
+                alt="აზომვითი ნახაზის მომზადება, ბინის შიდა აზომვა, აზომვითი სამუშაოები, ბინის აზომვა, სახლის აზომვა, ბინის აზომვითი ნახაზი, ნახაზების დამზადება, შიდა აზომვა, შიდა აზომვითი ნახაზი, ბინის გამიჯვნა, ბინის შეერთება, სარდაფის აზომვა, სხვენის აზომვა, ნახევარსარდაფის აზომვა, ავტოფარეხის აზომვა, საკადასტრო გეგმის მომზადება, საკადასტრო აზომვითი ნახაზი"
+                src="./assets/azomviti-naxazebi.png"
               />
             </div>
             <div className="description">
